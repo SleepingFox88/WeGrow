d50db6cb8ec104e8a273a88aad3a057440c27107
tsheltonking <intshelton@gmail.com>
2019-11-16 13:15:54 -0400

    First Features

--- contract-backend/contracts/WeGrow.sol
+++ contract-backend/contracts/WeGrow.sol
@@ -2,6 +2,27 @@ pragma solidity >=0.4.21 <0.6.0;
 
 contract WeGrow {
 
+    struct Grower { 
+        string name;
+        string location;
+        address account;
+    }
+
+    struct Item {
+        string name;
+        string location;
+        address owner;
+        uint price;
+        bool forSale;
+        int itemCount;
+    } 
+
+    uint public itemCount;
+    mapping (uint => Item) public itemList;
+    mapping (address => string) public location;
+
+
+}
     //
     // State variables
     //
